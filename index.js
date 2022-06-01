987b220cb244a6491e06e91d1844583dfb41dac6
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-06-01 07:20:30 -0700

    localStorage added

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const container = document.querySelector('.container');
-// const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 let total = document.getElementById('total');
 let count = document.getElementById('count');
 const movieSelect = document.getElementById('movieSelect');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
-function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll('.row .seat.selected');
-  const selectedSeatsCount = selectedSeats.length;
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
+function updateSelectedCountandTotal() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  console.log(seatIndex);
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  console.log(selectedSeats);
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
-  updateSelectedCount();
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCountandTotal();
 });
 
 container.addEventListener('click', (e) => {
@@ -25,6 +56,8 @@ container.addEventListener('click', (e) => {
     !e.target.classList.contains('occupied')
   ) {
     e.target.classList.toggle('selected');
-    updateSelectedCount();
+    updateSelectedCountandTotal();
   }
 });
+
+updateSelectedCountandTotal();
